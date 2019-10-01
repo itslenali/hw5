07c7ed75ebd6ba1cbc51d3d1489507af3599ec72
Lena Li <lenali@Lenas-MacBook-Pro.local>
2019-10-01 12:54:52 -0400

    added reactive elements onto full details page

--- fulldetails.js
+++ fulldetails.js
@@ -3,42 +3,55 @@ const redCircle = document.querySelector(".red-circle");
 const orangeCircle = document.querySelector(".orange-circle");
 const blueCircle = document.querySelector(".blue-circle");
 const text = document.querySelector(".color-text")
+const tinyText= document.querySelector(".tiny-text")
+const smallText= document.querySelector(".small-text")
+const mediumText= document.querySelector(".medium-text")
+const largeText= document.querySelector(".large-text")
+const shoppingCart= document.querySelector(".add-to-bag")
 
-blackCircle.addEventListener("mouseover", (e) => {
+
+
+
+
+blackCircle.addEventListener("click", (e) => {
 	text.textContent = "BLACKBERRY"
 })
 
 
-blackCircle.addEventListener("mouseout", (e) => {
-	text.textContent = ""
-})
-
-redCircle.addEventListener("mouseover", (e) => {
+redCircle.addEventListener("click", (e) => {
 	text.textContent = "STRAWBERRY"
 })
 
 
-redCircle.addEventListener("mouseout", (e) => {
-	text.textContent = ""
-})
-
-orangeCircle.addEventListener("mouseover", (e) => {
+orangeCircle.addEventListener("click", (e) => {
 	text.textContent = "FIRE ORANGE"
 })
 
 
-orangeCircle.addEventListener("mouseout", (e) => {
-	text.textContent = ""
-})
-
-blueCircle.addEventListener("mouseover", (e) => {
+blueCircle.addEventListener("click", (e) => {
 	text.textContent = "CRAZYBERRY"
 })
 
-
-blueCircle.addEventListener("mouseout", (e) => {
-	text.textContent = ""
+tinyText.addEventListener("click", (e) => {
+	tinyText.style.backgroundColor = "#E2E0E0"
 })
+
+smallText.addEventListener("click", (e) => {
+	smallText.style.backgroundColor = "#E2E0E0"
+})
+
+mediumText.addEventListener("click", (e) => {
+	mediumText.style.backgroundColor = "#E2E0E0"
+})
+
+largeText.addEventListener("click", (e) => {
+	largeText.style.backgroundColor = "#E2E0E0"
+})
+
+shoppingCart.addEventListener("click", (e) => {
+	largeText.style.backgroundColor = "#E2E0E0"
+})
+
 
 
 
